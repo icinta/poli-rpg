64b19ffd3a91a1f1a4439aa62505c38c32965447
icinta <andresmdavila97@gmail.com>
2020-04-16 13:22:13 -0400

    Workshop 1

--- src/main.py
+++ src/main.py
@@ -2,8 +2,16 @@ from hashlib import md5
 from termcolor import colored
 from os import system
 import json
+from store import *
 
 def main():
+   with open('ng.json','r') as f:
+      player_data = json.loads(f.read())
+
+      store = Store(player_data['player'],player_data['items'])
+   
+      store.printItems()
+
    print('Hello World!')
 
 main()
